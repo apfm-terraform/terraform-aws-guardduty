5082e19584f2b480a2a485a54e6a4ba7630421cb
marta <marta.susek@aplaceformom.com>
2024-07-09 12:45:48 +0200

    feat: restore original source in tests

--- test/organizations/delegated-admin/main.tf
+++ test/organizations/delegated-admin/main.tf
@@ -1,30 +1,28 @@
 data "aws_caller_identity" "current" {}
 
 module "delegated_admin" {
-  source = "../../../modules/organizations_admin"
-  # version = "0.0.2"
+  source  = "aws-ia/guardduty/aws//modules/organizations_admin"
+  version = "0.0.2"
 
   admin_account_id                 = data.aws_caller_identity.current.account_id
   auto_enable_organization_members = "NEW"
   guardduty_detector_id            = module.guardduty_detector.guardduty_detector.id
 
-  enable_s3_protection               = true
-  enable_kubernetes_protection       = true
-  enable_malware_protection          = true
-  auto_enable_eks_runtime_monitoring = "ALL"
+  enable_s3_protection         = true
+  enable_kubernetes_protection = true
+  enable_malware_protection    = true
 }
 
 module "guardduty_detector" {
-  source = "../../.."
-  # version = "0.0.2"
+  source  = "aws-ia/guardduty/aws"
+  version = "0.0.2"
 
   enable_guardduty = true
 
-  enable_s3_protection          = true
-  enable_kubernetes_protection  = true
-  enable_malware_protection     = true
-  enable_snapshot_retention     = true
-  enable_eks_runtime_monitoring = true
-  finding_publishing_frequency  = "FIFTEEN_MINUTES"
-  tags                          = {}
+  enable_s3_protection         = true
+  enable_kubernetes_protection = true
+  enable_malware_protection    = true
+  enable_snapshot_retention    = true
+  finding_publishing_frequency = "FIFTEEN_MINUTES"
+  tags                         = {}
 }
